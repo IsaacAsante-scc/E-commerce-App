eebdfe513a4466e93854c7d123a9ad18d56da693
Isaac Asante <W0725009@myscc.ca>
2022-04-19 03:40:48 -0400

    Display Clothing Items On Content View
    Display all products on content view using Product Card model.

--- E-commerce App/Views/ContentView.swift
+++ E-commerce App/Views/ContentView.swift
@@ -8,9 +8,15 @@
 import SwiftUI
 
 struct ContentView: View {
+    var columns = [GridItem(.adaptive(minimum: 160), spacing: 20)]
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        LazyVGrid(columns: columns, spacing: 20) {
+            ForEach(productList, id: \.id) { product in
+                ProductCard(product: product)
+            }
+        }
+        .padding()
     }
 }
 
