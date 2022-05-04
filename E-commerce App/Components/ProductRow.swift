6ddbf0bdd437c8e1c898b8dfde8d380d933cb0db
Isaac Asante <W0725009@myscc.ca>
2022-05-03 22:05:38 -0400

    Create Product Row Component
    Product row component created for items in bag. Delete button added to remove items from users bag. Note comments need to be added to ProductRow.

---
+++ E-commerce App/Components/ProductRow.swift
@@ -0,0 +1,48 @@
+//
+//  ProductRow.swift
+//  E-commerce App
+//
+//  Created by Isaac Asante on 2022-05-03.
+//
+
+import SwiftUI
+
+struct ProductRow: View {
+    @EnvironmentObject var bagManager: BagManager
+    var product: Product
+    
+    var body: some View {
+        HStack(spacing: 20) {
+            Image(product.image)
+                .resizable()
+                .aspectRatio(contentMode: .fit)
+                .frame(width: 50)
+                .cornerRadius(10)
+            
+            VStack(alignment: .leading, spacing: 10) {
+                Text(product.name)
+                    .bold()
+                
+                Text("$\(product.price)")
+            }
+            
+            Spacer()
+            
+            Image(systemName: "trash")
+                .foregroundColor(.red)
+                .onTapGesture {
+                    bagManager.removeFromBag(product: product)
+                }
+        }
+        .padding(.horizontal)
+        .frame(maxWidth: .infinity, alignment: .leading)
+    }
+}
+
+struct ProductRow_Previews: PreviewProvider {
+    static var previews: some View {
+        ProductRow(product: productList[3])
+            .environmentObject(BagManager())
+    
+    }
+}
